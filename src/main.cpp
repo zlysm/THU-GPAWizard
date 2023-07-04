9d2582862ea07dc3c5253c87c79598bd799f3dcc
Yiming Zhang <zym0622@outlook.com>
2023-07-04 17:51:14 +0800

    added save functions in main

--- src/main.cpp
+++ src/main.cpp
@@ -17,8 +17,9 @@ int main() {
     std::cout << "Stages:" << std::endl
               << "1. Student select courses" << std::endl
               << "2. Teacher set grades and push to Academic Affairs" << std::endl
-              << "3. Grade reconsideration" << std::endl
-              << "4. Show final course report" << std::endl;
+              << "3. Grade query and reconsideration" << std::endl
+              << "4. Show final course report" << std::endl
+              << "5. Save Students and Courses" << std::endl;
 
     // select courses
     std::cout << std::endl
@@ -48,13 +49,21 @@ int main() {
 
     // grade reconsideration
     std::cout << std::endl
-              << "---------Stage 3: Grade reconsideration--------" << std::endl
+              << "-------Stage 3: Query and Reconsideration------" << std::endl
               << std::endl;
+
     auto findStudent([&](int id) {
         for (auto& student : students)
             if (student->getID() == id)
                 return student;
         return (Student*)nullptr;
+    });
+
+    auto findCourse([&](int number) {
+        for (auto& course : courses)
+            if (course->getCourseInfo().number == number)
+                return course;
+        return (Course*)nullptr;
     });
 
     const std::string queryPrompt = "Please input the student's id you want to query(0 to finish): ";
@@ -69,13 +78,6 @@ int main() {
         }
         student->showCourseReport();
 
-        auto findCourse([&](int number) {
-            for (auto& course : courses)
-                if (course->getCourseInfo().number == number)
-                    return course;
-            return (Course*)nullptr;
-        });
-
         for (int courseNumber = IO::checkInput<int>(reconsiderPrompt); 0 != courseNumber;
              courseNumber = IO::checkInput<int>(reconsiderPrompt)) {
             auto course = findCourse(courseNumber);
@@ -84,9 +86,10 @@ int main() {
                 continue;
             }
 
-            course->setGrade(student->getID());
-            manager.pushGrade(courseNumber);
-            std::cout << "Successfully pushed grade to Academic Affairs." << std::endl;
+            if (course->setGrade(student->getID())) {
+                manager.pushGrade(courseNumber);
+                std::cout << "Successfully pushed grade to Academic Affairs." << std::endl;
+            }
         }
         std::cout << "Student " << student->getName() << " reconsideration finished." << std::endl
                   << std::endl;
@@ -98,6 +101,56 @@ int main() {
               << std::endl;
     for (const auto& student : students)
         student->showCourseReport();
+
+    // save students and courses
+    std::cout << std::endl
+              << "--------Stage 5: Save Students/Courses---------" << std::endl
+              << std::endl;
+
+    const std::string savePrompt =
+        "Select the sorting method you want(default is 0): \n"
+        "1. Name\n"
+        "2. ID\n"
+        "3. GPA\n";
+
+    choice = IO::checkInput<char>("Do you want to save all " + std::to_string(Student::getStudentCount()) + " students? (y/n)");
+    if ('n' != choice && 'N' != choice) {
+        int sortType = IO::checkInput<int>(savePrompt);
+        manager.saveStudents(static_cast<IO::SortType>(sortType));
+        std::cout << std::endl;
+    }
+
+    choice = IO::checkInput<char>("Do you want to save a student's courses info? (y/n)");
+    if ('n' != choice && 'N' != choice) {
+        const std::string studentPrompt = "Please input the student's id(0 to finish): ";
+        for (int studentID = IO::checkInput<int>(studentPrompt); 0 != studentID;
+             studentID = IO::checkInput<int>(studentPrompt)) {
+            if (!findStudent(studentID)) {
+                std::cout << "Student not found." << std::endl;
+                continue;
+            }
+            int sortType = IO::checkInput<int>(savePrompt);
+            manager.saveStudents(studentID, static_cast<IO::SortType>(sortType));
+            std::cout << std::endl;
+        }
+    }
+
+    choice = IO::checkInput<char>("Do you want to save a course's students info? (y/n)");
+    if ('n' != choice && 'N' != choice) {
+        const std::string coursePrompt = "Please input the course number(0 to finish): ";
+        for (int courseNumber = IO::checkInput<int>(coursePrompt); 0 != courseNumber;
+             courseNumber = IO::checkInput<int>(coursePrompt)) {
+            if (!findCourse(courseNumber)) {
+                std::cout << "Course not found." << std::endl;
+                continue;
+            }
+            int sortType = IO::checkInput<int>(savePrompt);
+            manager.saveCourses(courseNumber, static_cast<IO::SortType>(sortType));
+            std::cout << std::endl;
+        }
+    }
+
+    std::cout << "---------------Thanks for using!---------------" << std::endl;
 
     return 0;
 }
